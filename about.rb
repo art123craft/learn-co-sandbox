cac3fa1390f863b3e5b5a3cacb96a3be4e73295f
art123craft <cammiebluecammie@gmail.com>
2018-08-01 15:10:39 +0000

    automatically backed up by learn

---
+++ about.rb
@@ -0,0 +1,11 @@
+# def about_me
+#   puts "My name is Camille."
+#   puts "I like art and crafts."
+#   puts "I'm 13 years old."
+#   puts " My favorite food is anything grilled, except vegggies."
+# end
+# about_me
+
+ def four_countries ["Chad","Cuba","Greenland","Iraq","Mali","Oman","India"]
+puts four_countries.push ("Fiji"
+end
--- interpolation.rb
+++ interpolation.rb
@@ -2,3 +2,7 @@ name = "Camille"
 best_pizza = "Meat Lovers"
 
 puts "Hi! My name is #{name} and my favorite pizza is #{best_pizza}."
+
+name = "Camille"
+age = "13"
+puts "My name is " + name + " and I am " + age.to_s +  " years old "
